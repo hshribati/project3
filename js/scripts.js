f8306a6dc600cf45af37e6faaa768eedd98cdf4c
hshribati <158009247+hshribati@users.noreply.github.com>
2024-11-11 01:22:58 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-// JavaScript Picture Slider
-let slideIndex = 0;
+// Initialize the map when the API is loaded
+function initMap() {
+  var location = {lat: -34.397, lng: 150.644};  // Change this to your desired location
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+// Ensure the API script callback is set to initialize the map
+window.onload = function() {
+  if (typeof initMap === 'function') {
+    // The callback to initMap will be automatically called by the Google Maps API
   }
-  
-  slideIndex++;
-  
-  if (slideIndex > slides.length)
+}
